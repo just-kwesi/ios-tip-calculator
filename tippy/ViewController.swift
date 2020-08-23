1299b07272e4d2c709876e9a6f082f0d33f97286
Frederick Tetteh <kwesiossom@gmail.com>
2020-08-23 17:54:17 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Frederick Tetteh on 8/23/20.
+//  Copyright © 2020 Frederick Tetteh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
